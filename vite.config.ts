27c85bb43199e82e0451b818902b613003333ebf
Theo Browne <me@t3.gg>
2022-08-09 02:43:15 -0700

    vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 
+import vercel from "solid-start-vercel";
+
 export default defineConfig({
   plugins: [
     solid({
       ssr: false,
+      adapter: vercel(),
     }),
   ],
 });
