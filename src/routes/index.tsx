b5f396e82f8265a519e3b8c39d342e44664b7857
Theo Browne <me@t3.gg>
2022-08-10 00:27:09 -0700

    homepage kinda

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,10 @@ export default function Chat() {
   return (
     <div class="h-screen overflow-y-scroll">
       <div class="flex flex-col w-96 bg-gray-200 text-lg p-4">
-        Chat app - wrong url tho
+        Chat app - add channel name after the / i.e.{" "}
+        <a href="/theo">{`${
+          typeof window !== undefined ? window.location.href : ""
+        }theo`}</a>
       </div>
     </div>
   );
