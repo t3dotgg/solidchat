d0d3f30d8ba9907b65f0c20b7bef4fe9b7c2dd1c
Theo Browne <me@t3.gg>
2022-08-09 19:32:01 -0700

    kill TLA

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,49 +1,11 @@
-import type { ChatUserstate } from "tmi.js";
 import { createSignal, For, onMount } from "solid-js";
-
-type Message = {
-  user: ChatUserstate;
-  body: string;
-};
-
-const [signal, setSignal] = createSignal<Message[]>([]);
-
-if (typeof window !== undefined) {
-  const tmi = await import("tmi.js");
-  const client = new tmi.Client({
-    channels: ["#xqc"],
-  });
-
-  client.connect();
-
-  client.on("message", (channel, userstate, message) =>
-    setSignal((prev) => [...prev, { user: userstate, body: message }])
-  );
-}
 
 export default function Chat() {
   return (
     <div class="h-screen overflow-y-scroll">
       <div class="flex flex-col w-96 bg-gray-200 text-lg p-4">
-        <For each={signal()} fallback={<div />}>
-          {(item) => <Message message={item} />}
-        </For>
+        Chat app - wrong url tho
       </div>
     </div>
   );
 }
-
-const Message = ({ message }: { message: Message }) => {
-  onMount(() => document.getElementById(message.user.id)?.scrollIntoView());
-  return (
-    <div class="animate-fade-in" id={message.user.id}>
-      <span
-        class="font-bold"
-        style={{ color: message.user.color ?? "#000000" }}
-      >
-        {message.user["display-name"]}:{" "}
-      </span>
-      {message.body}
-    </div>
-  );
-};
