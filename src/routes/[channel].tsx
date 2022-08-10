8f8b5e44b6ec408929b4641f6413b66dc1512909
Theo Browne <me@t3.gg>
2022-08-09 22:12:59 -0700

    Emojis work but in a sketchy way

--- src/routes/[channel].tsx
+++ src/routes/[channel].tsx
@@ -18,11 +18,48 @@ const startChat = async (channel: string) => {
 
     client.connect();
 
-    client.on("message", (channel, userstate, message) =>
-      setSignal((prev) => [...prev, { user: userstate, body: message }])
-    );
+    client.on("message", (channel, userstate, message) => {
+      console.log("state", userstate);
+      setSignal((prev) => [...prev, { user: userstate, body: message }]);
+    });
   }
 };
+
+function getMessageHTML(message, { emotes }) {
+  if (!emotes) return message;
+
+  // store all emote keywords
+  // ! you have to first scan through
+  // the message string and replace later
+  const stringReplacements = [];
+
+  // iterate of emotes to access ids and positions
+  Object.entries(emotes).forEach(([id, positions]) => {
+    // use only the first position to find out the emote key word
+    const position = positions[0];
+    const [start, end] = position.split("-");
+    const stringToReplace = message.substring(
+      parseInt(start, 10),
+      parseInt(end, 10) + 1
+    );
+
+    stringReplacements.push({
+      stringToReplace: stringToReplace,
+      replacement: `<img src="https://static-cdn.jtvnw.net/emoticons/v1/${id}/3.0" style="height: 1.2rem; display: inline;" />`,
+    });
+  });
+
+  // generate HTML and replace all emote keywords with image elements
+  const messageHTML = stringReplacements.reduce(
+    (acc, { stringToReplace, replacement }) => {
+      // obs browser doesn't seam to know about replaceAll
+      return acc.split(stringToReplace).join(replacement);
+    },
+    message
+  );
+
+  return messageHTML;
+}
 
 export default function Chat() {
   const params = useParams();
@@ -49,7 +86,7 @@ const Message = ({ message }: { message: Message }) => {
       >
         {message.user["display-name"]}:{" "}
       </span>
-      {message.body}
+      <span innerHTML={getMessageHTML(message.body, message.user)} />
     </div>
   );
 };
